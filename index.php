83213002c7bde85c22ede237e6235bdc81e6010e
Strix Windows <anibal@strix.com.ve>
2020-07-03 16:25:35 -0400

    comments and starting the translator

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <HTML>
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
 <head>
-<title>Demo PHP IntTFHKA</title>
+<!-- <title>Demo PHP IntTFHKA</title> -->
+<title>Experimento de momento para strix</title>
 </head>
 <BODY>
 <div align = "center"><br>
@@ -23,22 +24,22 @@
             <input name ="EnviarComando" type = "submit"  value="SetPort" /></br></br>
 Comando: <input type = "text" name = "Comando" />
 <input name ="EnviarComando" type = "submit" value = "Enviar" /></br></br>
-<input name ="EnviarComando" type = "submit"  value="SubirS1" />
+<!-- <input name ="EnviarComando" type = "submit"  value="SubirS1" />
 <input name ="EnviarComando" type = "submit"  value="SubirS2" />
 <input name ="EnviarComando" type = "submit"  value="SubirS3"  />
 <input name ="EnviarComando" type = "submit"  value="SubirS4" />
 <input name ="EnviarComando" type = "submit"  value="SubirS5" />
 <input name ="EnviarComando" type = "submit"  value="SubirS6"  />
 <input name ="EnviarComando" type = "submit"  value="SubirU0X" />
-<input name ="EnviarComando" type = "submit"  value="SubirU0Z" />
+<input name ="EnviarComando" type = "submit"  value="SubirU0Z" /> -->
 </br>
 </br>
 <input name ="EnviarComando" type = "submit"  value="Facturar" />
-<input name ="EnviarComando" type = "submit"  value="Devolucion" />
+<!-- <input name ="EnviarComando" type = "submit"  value="Devolucion" /> -->
 </br>
 </br>
-<input name ="EnviarComando" type = "submit"  value="ReporteX" />
-<input name ="EnviarComando" type = "submit"  value="ReporteZ" />
+<!-- <input name ="EnviarComando" type = "submit"  value="ReporteX" />
+<input name ="EnviarComando" type = "submit"  value="ReporteZ" /> -->
 </form>
 </div>
 <?php
@@ -72,19 +73,20 @@ if (isset($Foperacion)){
 			$out =  $itObj->UploadReportCmd("U0Z" , "ReportData.txt");
 	  }elseif ($Foperacion == "Facturar") {
 	        $factura = array(
-							-5 => "iF*0000001\n",
-							-4 => "iI*Z4A1234567\n",
-				            -3 => "iD*18-01-2014\n",
-							-2 => "iS*Pedro Mendez\n",
-							-1 => "iR*12.345.678\n",
-							 0 => "!000000580000005000Harina\n",
+							// -5 => "iF*0000001\n",//factura asociadaj
+							// -4 => "iI*Z4A1234567\n",// numero de control de esa factura
+				            // -3 => "iD*18-01-2014\n",//fecha factura dia especifico
+							// -2 => "iS*Pedro Mendez\n", // mombre persona
+							// -1 => "iR*12.345.678\n", // rif
+							 0 => "!100000580910000512Harina\n",
 							 1 => "!000000150000001500Jamon\n",
 							 2 => "\"000000205000003000Patilla\n",
 							 3 => "#000005000000001000Caja de Whisky\n",
 							 4 => "#000005000000001000Caja de Chocolates\n",
 							 5 => "!000001000000004000Maracas de Peltre\n",
 							 6 => "\"000001000000004000Maracas de Aluminio\n",
-							 7 => "101"
+							 7 => " 000001000000004000Maracas Pesadas\n",
+							 8 => "101"
 							 );
 		$file = "Factura.txt";	
                 $fp = fopen($file, "w+");
@@ -143,7 +145,79 @@ if (isset($Foperacion)){
       echo "<div align = 'center'>".$out."</div>";
    }
    	  
-    //echo "<br><br><div align = 'center'>".$itObj->Log."</div>";
+	//echo "<br><br><div align = 'center'>".$itObj->Log."</div>";
+	
+
+	
+
+
+	// echo translateLine("X","125,00","8","bollitos de canela");
+
+	// // Ejemplos comandos:
+	// //detalle de 1
+	// // ! 1000005809 10000512 Harina
+	// // (!) - tasa fiscal
+	// // 10.000.058,09 - precio max cant de cifras 2 dec
+	// // 10.000,512 - cant max cifras 3 decimales
+	// // Harina - descripcion x digitos
+	
+	// // Ejemplos comandos: (otros a deconstruir)
+	// // !100000580900000512Harina
+	// // !000000150000001500Jamon
+	// // "000000205000003000Patilla
+	// // #000005000000001000Caja de Whisky
+	// // #000005000000001000Caja de Chocolates
+	// // !000001000000004000Maracas de Peltre
+	// // "000001000000004000Maracas de Aluminio
+	// // 101
+	
+	// function translateTasa($tasa="", $tipo_doc=""){
+	//   // de momento tengo entendido 4 tipos de tasa
+	//   // (falta copiar de manuak, pero en ejemplo tengo)// (!), ("), (#), ( )
+	
+	//  $tasas = ["\"","!","#"," "];
+	//  $tasa = $tasas[array_rand($tasas, 1)];
+	
+	//  $comando = $tasa;
+	 
+	//  return  $comando;
+	// }
+	
+	// function translatePrecio( $precio = ""){
+	
+	
+	//   //validaciones de tipo precio
+	//   // Precio del ítem (8 enteros + 2 decimales)
+	
+	
+	//  $comando =  $precio;
+	 
+	//  return  $comando;
+	// }
+	
+	// function translateCantidad($cant = ""){
+	
+	//  $comando = " cantidad";
+	 
+	//  return  $comando;
+	// }
+	
+	// function translateDescription($desc = ""){
+	
+	//  $comando = " descripcion";
+	 
+	//  return  $comando;
+	// }
+	
+	
+	// function translateLine( $tasa="", $precio = "", $cant = "", $desc = "",$tipo_doc=""){
+	
+	//  $comando = translateTasa($tasa, $tipo_doc) .translatePrecio($precio) . translateCantidad($cant) .translateDescription($desc);
+	
+	//  return  $comando;
+	// }
+
+
 }
 	
 ?>
