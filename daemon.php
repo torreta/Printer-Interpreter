15c7adf647b871483d0a56cbd2a38c3cb0c909a7
Strix Windows <anibal@strix.com.ve>
2020-07-15 16:04:26 -0400

    now, dirt included

--- daemon.php
+++ daemon.php
@@ -333,7 +333,6 @@ while (true) {
 
 
   // PASITO A PASITO
-
   // (0) verifico si hay facturas en la tabla current (1)
 
   // (1) (true) de haber tomo el documento y lo imprimo (...desarrollar ++) (3)
@@ -357,6 +356,163 @@ while (true) {
   // ... se coloca el mensaje en la tabla log de mensajes (END)
 
 
+
+  // if (  $result->num_rows > 0) { //significa que hay almenos 1 factura
+
+  //   // output data of each row
+  //  /*  while($row = $result->fetch_assoc()) {
+  //     echo "\n";
+  //     echo "factura de id: " . $row["invoice_id"]." esta imprimiendo acturalmente;";
+  //     echo "\n";
+  //     // id de la factura en cuestion
+  //     $invoice_id = $row["invoice_id"];
+
+  //     $items_factura = "SELECT * from dbo_printer_current WHERE printer_id = ".PRINTER_ID.";";
+
+  //     $result = $conn->query($items_factura); */
+
+
+  //     // if (count($items_factura) > 0) {
+  //     //   // output data of each row
+  //     //   while($row = $items_factura->fetch_assoc()) {
+  //     //     echo "\n";
+  //     //     echo "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br>";
+  //     //     echo "\n";
+
+  //     //     // $factura[$index_counter] = "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br> \n";
+  //     //     $factura[$index_counter] = translateLine("X",$row["price"],$row["quantity"],$row["description"])."\n";
+  //     //     $index_counter++;
+  //     //   }
+
+  //     //   //cierre de factura
+  //     //   $factura[$index_counter] = "101";
+  //     //       echo "\n";
+  //     //       echo "factura generada";
+  //     //       echo "\n";
+
+  //     //       var_dump( $factura); 
+
+  //     //     //   $file = "Factura.txt";  
+  //     //     //     $fp = fopen($file, "w+");
+  //     //     //     $write = fputs($fp, "");
+                              
+  //     //     //   foreach($factura as $campo => $cmd)
+  //     //     //   {
+  //     //     //     $write = fputs($fp, $cmd);
+  //     //     //   }
+                              
+  //     //     // fclose($fp); 
+
+  //     //     //revisar resultados de dispositivo
+  //     //     // $out =  $itObj->SendFileCmd($file);
+  //     //     // var_dump($out);
+
+
+  //     // } else {
+  //     //   echo "0 results for items";
+  //     //   // log para algo serio
+  //     // }
+      
+  //   }
+
+  //     //cerrando db
+  //      $conn->close();
+
+  
+  //   // while($row = $result->fetch_assoc()) {
+  //   //   echo "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br>";
+  //   //   echo "\n";
+
+  //   //   // $factura[$index_counter] = "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br> \n";
+  //   //   $factura[$index_counter] = translateLine("X",$row["price"],$row["quantity"],$row["description"])."\n";
+  //   //   $index_counter++;
+  //   // }
+
+  //   // if ($result->num_rows > 0) {
+  //   //   // output data of each row
+  //   //   while($row = $result->fetch_assoc()) {
+  //   //     echo "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br>";
+  //   //     echo "\n";
+
+  //   //     // $factura[$index_counter] = "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br> \n";
+  //   //     $factura[$index_counter] = translateLine("X",$row["price"],$row["quantity"],$row["description"])."\n";
+  //   //     $index_counter++;
+  //   //   }
+
+  //   //   //cierre de factura
+  //   //   $factura[$index_counter] = "101";
+
+  //   // } else {
+  //   //   echo "0 results";
+  //   // }
+
+
+
+  //   // $_query = $_PDO->prepare("
+  //   //       SELECT
+  //   //         dbo_administration_invoices_items.id,
+  //   //         dbo_administration_invoices_items.invoice_id,
+  //   //         dbo_administration_invoices_items.price,
+  //   //         dbo_administration_invoices_items.quantity, 
+  //   //         dbo_administration_invoices_items.tax_id,
+  //   //         dbo_config_taxes.percentage,
+  //   //         dbo_config_taxes.observation,
+  //   //         dbo_administration_invoices_items.exchange_rate_id,
+  //   //         dbo_config_exchange_rates.exchange_rate,
+  //   //         dbo_config_currencies.abbreviation,
+  //   //         dbo_config_currencies.`name`,
+  //   //         dbo_storage_products.`code`,
+  //   //         dbo_storage_products.description
+  //   //       FROM `dbo_administration_invoices_items`
+  //   //       join dbo_config_taxes on dbo_administration_invoices_items.tax_id = dbo_config_taxes.id
+  //   //       join dbo_config_exchange_rates on dbo_administration_invoices_items.exchange_rate_id = dbo_config_exchange_rates.id
+  //   //       join dbo_config_currencies on dbo_config_exchange_rates.currency_id = dbo_config_currencies.id
+  //   //       join dbo_storage_products on dbo_administration_invoices_items.product_id = dbo_storage_products.id
+  //   //       WHERE 	dbo_administration_invoices_items.invoice_id = " .."
+  //   //       ;
+  //   //     ");
+  //   // $_query->execute();
+  //   // $facturas = $_query->fetchAll();
+
+
+
+
+
+  //   // while($row = $result->fetch_assoc()) {
+  //   //   echo "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br>";
+  //   //   echo "\n";
+
+  //   //   // $factura[$index_counter] = "price: " . $row["price"]. " - quantity: " . $row["quantity"]. ", description " . $row["description"]. "<br> \n";
+  //   //   $factura[$index_counter] = translateLine("X",$row["price"],$row["quantity"],$row["description"])."\n";
+  //   //   $index_counter++;
+  //   // }
+
+  //   // //cierre de factura
+  //   // $factura[$index_counter] = "101";
+
+  // } else {
+  //   echo "nothing currently printing, cheking pending";
+
+  //   //busco pendientes
+
+  // }
+
+  // //     var_dump( $factura); 
+
+  // //     $file = "Factura.txt";  
+  // //       $fp = fopen($file, "w+");
+  // //       $write = fputs($fp, "");
+                        
+  // //     foreach($factura as $campo => $cmd)
+  // //     {
+  // //       $write = fputs($fp, $cmd);
+  // //     }
+                        
+  // //   fclose($fp); 
+
+  //   //revisar resultados de dispositivo
+  //   // $out =  $itObj->SendFileCmd($file);
+  //   // var_dump($out);
 
 
   // Sleep
