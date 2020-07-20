5f78335d63b95439ceef28a1fda9b375ac0ad454
Strix Windows <anibal@strix.com.ve>
2020-07-20 11:44:56 -0400

    updating printer message and doing the same for the log, will make it so the error only prints once on log but updates on messages

--- daemon.php
+++ daemon.php
@@ -260,43 +260,127 @@ while (true) {
               }
 
               // ... se sobrescribe el mensaje para la impresora de mensajes 
-              $mensaje_al_log = "la factura " . $numero_factura .", por cajero ". $nombre_cajero. "ha impreso con exito.";
+              $mensaje_al_log = "la factura " . $numero_factura .", por cajero ". $nombre_cajero. ", ha impreso con exito.";
 
               $query_update_message = 
-              "INSERT OR UPDATE INTO dbo_printer_messages(
-                    id, 
-                    message, 
-                    printer_id, 
-                    user_id, 
-                    cashier_name) 
+              "INSERT dbo_printer_messages(
+                  id, 
+                  message, 
+                  printer_id, 
+                  user_id, 
+                  cashier_name) 
                 VALUES ("
-                .$documento_imprimiendo["printer_id"].","
-                .$mensaje_al_log.", "
-                .$documento_imprimiendo["printer_id"].", "
-                .$documento_imprimiendo["user_id"].", '"
-                .$documento_imprimiendo["cashier_name"]."');";
+                  .$documento_imprimiendo["printer_id"].",'"
+                  .$mensaje_al_log."', "
+                  .$documento_imprimiendo["printer_id"].", "
+                  .$documento_imprimiendo["user_id"].", '"
+                  .$documento_imprimiendo["cashier_name"]."')
+                ON DUPLICATE KEY UPDATE 
+                  message = '".$mensaje_al_log."'
+                ;";
 
               // puedes validar el query aca
               // echo ( $query_update_message );
 
               $actualizar_mensaje_impresora = $conn->prepare($query_update_message);
 
-            if ($actualizar_mensaje_impresora->execute()) {
-              echo "se ha actualizado el mensaje de la impresora. \n";
-            } else {
+              if ($actualizar_mensaje_impresora->execute()) {
+                echo "se ha actualizado el mensaje de la impresora. \n";
+              } else {
+                echo "(al actualizar mensaje de la impresora) Error: " . $sql . "\n" . mysqli_error($conn);
+              }
+              
+              // ... se coloca el mensaje en la tabla log de mensajes (END)
+              $query_log_message = 
+              "INSERT INTO dbo_printer_log(
+                  message, 
+                  printer_id, 
+                  user_id, 
+                  cashier_name) 
+              VALUES ('"
+              .$mensaje_al_log."', "
+              .$documento_imprimiendo["printer_id"].", "
+              .$documento_imprimiendo["user_id"].", '"
+              .$documento_imprimiendo["cashier_name"]."');";
+
+              // puedes validar el query aca
+              // echo ( $query_log_message );
+
+              $actualizar_mensaje_impresora = $conn->prepare($query_log_message);
+
+              if ($actualizar_mensaje_impresora->execute()) {
+              echo "se ha escrito un registro nuevo en log de impresiones.  \n";
+              } else {
               echo "(al actualizar mensaje de la impresora) Error: " . $sql . "\n" . mysqli_error($conn);
-            }
-            
-            // ... se coloca el mensaje en la tabla log de mensajes (END)
+              }
+
 
             }else{
               // (3) (false)  verifico el mensaje del controlador al imprimir, (condiciones de parseo), si sale un error
+              
               // ... se mantiene la factura en current (sin cambios)
-              // ... se sobrescribe el mensaje para la impresora de mensajes, indicando que hay un error
-              // ... se coloca el mensaje en la tabla log de mensajes (END)
+
               echo "la impresora fallo... (hay que colocar los errores en log)\n";
               // ... busco en checkprinter cual puede ser la razon del error.
 
+
+
+              // ... se sobrescribe el mensaje para la impresora de mensajes, indicando que hay un error
+              $mensaje_al_log = "la factura " . $numero_factura .", por cajero ". $nombre_cajero. ", tiene problemas para imprimir...";
+                
+              $query_update_message = 
+              "INSERT dbo_printer_messages(
+                  id, 
+                  message, 
+                  printer_id, 
+                  user_id, 
+                  cashier_name) 
+                VALUES ("
+                  .$documento_imprimiendo["printer_id"].",'"
+                  .$mensaje_al_log."', "
+                  .$documento_imprimiendo["printer_id"].", "
+                  .$documento_imprimiendo["user_id"].", '"
+                  .$documento_imprimiendo["cashier_name"]."')
+                ON DUPLICATE KEY UPDATE 
+                  message = '".$mensaje_al_log."'
+                ;";
+
+              // puedes validar el query aca
+              // echo ( $query_update_message );
+
+              $actualizar_mensaje_impresora = $conn->prepare($query_update_message);
+
+              if ($actualizar_mensaje_impresora->execute()) {
+                echo "se ha actualizado el mensaje de la impresora, con el mensaje de error. \n";
+              } else {
+                echo "(al actualizar mensaje de la impresora)(error impresion) Error: " . $sql . "\n" . mysqli_error($conn);
+              }
+              
+              // ... se coloca el mensaje en la tabla log de mensajes (END)
+              $query_log_message = 
+              "INSERT INTO dbo_printer_log(
+                  message, 
+                  printer_id, 
+                  user_id, 
+                  cashier_name) 
+              VALUES ('"
+              .$mensaje_al_log."', "
+              .$documento_imprimiendo["printer_id"].", "
+              .$documento_imprimiendo["user_id"].", '"
+              .$documento_imprimiendo["cashier_name"]."');";
+
+              // puedes validar el query aca
+              // echo ( $query_log_message );
+
+              $actualizar_mensaje_impresora = $conn->prepare($query_log_message);
+
+              if ($actualizar_mensaje_impresora->execute()) {
+              echo "se ha escrito un registro nuevo en log de impresiones.  \n";
+              } else {
+              echo "(al actualizar mensaje de la impresora)(error impresion)Error: " . $sql . "\n" . mysqli_error($conn);
+              }
+
+              
             }
 
           } else {
