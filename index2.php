87509d1018184ac273f57847e23c8a4acb4aafbf
Strix Windows <anibal@strix.com.ve>
2020-07-31 16:14:25 -0400

    files made for testing nofiscal printing stuff

--- index2.php
+++ index2.php
@@ -1,3 +1,10 @@
 <?php
-    shell_exec("IntTFHKA.exe SendFileCmd(Factura.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(Comando.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(Impuesto.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(Factura.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(Retiro.txt");
+    shell_exec("IntTFHKA.exe SendFileCmd(NoFiscal.txt");
+
+
+
 ?>
