a3dff940ba28d1fdcf48b82731756264de6cdd54
Strix Windows <anibal@strix.com.ve>
2020-09-22 10:44:01 -0400

    adding new line

--- index2.php
+++ index2.php
@@ -2,8 +2,9 @@
     // shell_exec("IntTFHKA.exe SendFileCmd(Comando.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(Impuesto.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(Factura.txt");
+    shell_exec("IntTFHKA.exe SendFileCmd(Debito.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(Retiro.txt");
-    shell_exec("IntTFHKA.exe SendFileCmd(NoFiscal.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(NoFiscal.txt");
 
 
 
