7ffd3b0e45b0edde1b51127975abf088c7c82a62
Luis Campos <luiscampostorreta@gmail.com>
2022-03-23 22:35:54 +0100

    reseting file as normal

--- index2.php
+++ index2.php
@@ -1,12 +1,11 @@
 <?php
     // shell_exec("IntTFHKA.exe SendFileCmd(Comando.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(Impuesto.txt");
-    // shell_exec("IntTFHKA.exe SendFileCmd(Factura.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(FacturaManual.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(Debito.txt");
-    shell_exec("IntTFHKA.exe SendFileCmd(Credito.txt");
+    // shell_exec("IntTFHKA.exe SendFileCmd(Credito.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(Retiro.txt");
     // shell_exec("IntTFHKA.exe SendFileCmd(NoFiscal.txt");
-
-
+    shell_exec("IntTFHKA.exe SendFileCmd(Test.txt");
 
 ?>
