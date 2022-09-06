ccf5f00173705e318f7051f26ee5982c01106be1
Luis Campos <luiscampostorreta@gmail.com>
2022-09-06 19:08:51 -0400

    examples so i can have it noted

---
+++ CONEXIONES/DBConfig Los Cocos.php
@@ -0,0 +1,13 @@
+<?php
+  /*** (B) SETTINGS ***/
+  // Database settings 
+  // change these to your own
+  // Do a copy of this file without the .example as namefile
+  define('DB_HOST', '192.168.1.107');
+  define('DB_NAME', 'pos_development');
+  define('DB_PORT', '3306');
+  define('DB_CHARSET', 'utf8');
+  define('DB_USER', 'cocoprinter');
+  define('DB_PASSWORD', 'Manager1.');
+  
+?>
